50781932f75f9581d1a212ae33cee4d62b875e0b
pmita <panos.mita2@outlook.com>
2021-11-17 14:20:24 +0000

    Feature/Signup page template added

--- src/pages/signup/Signup.js
+++ src/pages/signup/Signup.js
@@ -1,11 +1,87 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Signup.css';
 
 const Signup = () => {
+    //STATE
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [displayName, setDisplayName] = useState('');
+    // eslint-disable-next-line no-unused-vars
+    const [thumbnail, setThumbnail] = useState(null);
+    const [thumbnailError, setThumbnailError] = useState(null);
+    //EVENTS
+    const handleSubmit = (e) =>{
+        e.preventDefault();
+        console.table(email, password, displayName, thumbnail, thumbnailError)
+    }
+
+    const handleFileChange = (e) => {
+        setThumbnail(null); // Reset State
+
+        let selected = e.target.files[0]; // Returns an array of files, for this application we only expect 1 file
+        console.log(selected);
+
+        // Check the user has selected a file, that matches size and type limits
+        if(!selected){
+            setThumbnailError('Please select a file!');
+            return;
+        }
+
+        if(!selected.type.includes('image')){
+            setThumbnailError('Selected file must ne an image!');
+            return;
+        }
+
+        if(!selected.size > 100000){
+            setThumbnailError('Image file size must be less that 100kb');
+            return;
+        }
+
+        setThumbnailError(null);
+        setThumbnail(selected);
+        console.log('Thumbnail updated');
+    }
     return(
-        <div>
-            Welcome to Signup page component
-        </div>
+        <form onSubmit={handleSubmit} className='auth-form'>
+            <h2>Sign up</h2>
+            <label>
+                <span>Email:</span>
+                <input 
+                    required
+                    type='email'
+                    onChange={(e) => setEmail(e.target.value)}
+                    value={email}
+                />
+            </label>
+            <label>
+                <span>Password:</span>
+                <input 
+                    required
+                    type='password'
+                    onChange={(e) => setPassword(e.target.value)}
+                    value={password}
+                />
+            </label>
+            <label>
+                <span>Display Name:</span>
+                <input 
+                    required
+                    type='text'
+                    onChange={(e) => setDisplayName(e.target.value)}
+                    value={displayName}
+                />
+            </label>
+            <label>
+                <span>Profile Thumbnail:</span>
+                <input 
+                    required
+                    type='file'
+                    onChange={handleFileChange}
+                />
+                {thumbnailError && <div className='error'>{thumbnailError}</div>}
+            </label>
+            <button className='btn'>Sign Up</button>
+        </form>
     );
 }
 
