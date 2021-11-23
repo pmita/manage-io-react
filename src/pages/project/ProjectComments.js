1b42e91988612a6e18abe1a044762e22c39bdc5f
pmita <panos.mita2@outlook.com>
2021-11-23 21:22:48 +0000

    Feature/Dynamic timestamps added

--- src/pages/project/ProjectComments.js
+++ src/pages/project/ProjectComments.js
@@ -5,6 +5,8 @@ import { timestamp } from '../../firebase/config';
 //HOOKS
 import { useAuthContext } from '../../hooks/useAuthContext';
 import { useFirestore } from '../../hooks/useFirestore';
+//ASSETS
+import formatDistanceToNow from 'date-fns/formatDistanceToNow';
 
 const ProjectComments = ({ project }) => {
     //STATE
@@ -43,7 +45,7 @@ const ProjectComments = ({ project }) => {
                             <p>{comment.displayName}</p>
                         </div>
                         <div className='comment-date'>
-                            <p>date here</p>
+                            <p>{formatDistanceToNow(comment.createdAt.toDate(), { addSuffix : true })}</p>
                         </div>
                         <div className='comment-content'>
                             <p>{comment.content}</p>
